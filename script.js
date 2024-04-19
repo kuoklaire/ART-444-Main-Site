4d8c4f3a3ba6926c7b6d42145ceead36c1fb063f
Claire Kuo <Kuoclaire0@gmail.com>
2024-04-18 20:58:51 -0700

    JavaScript build

--- script.js
+++
@@ -1,6 +0,0 @@
-<script>
-    function myMenu() {
-        document.getElementById("mymenu").classList.toggle("open");
-        document.getElementById('menuButton').classList.toggle('close');
-    }
-</script>
